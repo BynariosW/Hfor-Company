fc2011bd6c4ad50cbf3f50935744435d17bd4b5c
xXAgu123450Xx <agustin_123450@hotmail.com>
2017-08-18 18:48:33 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+	var preloader = document.getElementById('preloader-container');
+		
+	preloader.style.visibility = "hidden";
+	preloader.style.opacity = "0";
+
+	var body = document.getElementById('body');
+
+	body.style.overflow = "inherit"
+}
